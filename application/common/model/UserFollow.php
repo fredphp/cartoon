13203de4500d5e2cf21ed58a6cc06c98b8a468fe
fred <fredphp52@gmail.com>
2026-04-13 12:53:45 +0800

    关注

--- application/common/model/UserFollow.php
+++ application/common/model/UserFollow.php
@@ -28,13 +28,68 @@ class UserFollow extends Model
 
     ];
     
+    /**
+     * 关联被关注的用户（作者）
+     */
+    public function followUser()
+    {
+        return $this->belongsTo('User', 'follow_user_id', 'id');
+    }
 
+    /**
+     * 关联关注者
+     */
+    public function user()
+    {
+        return $this->belongsTo('User', 'user_id', 'id');
+    }
+
+    /**
+     * 判断用户是否已关注某作者
+     *
+     * @param int $userId
+     * @param int $followUserId
+     * @return bool
+     */
+    public static function hasFollowed($userId, $followUserId)
+    {
+        return self::where('user_id', $userId)
+            ->where('follow_user_id', $followUserId)
+            ->count() > 0;
+    }
+
+    /**
+     * 获取用户关注的作者ID列表
+     *
+     * @param int $userId
+     * @return array
+     */
+    public static function getFollowedIds($userId)
+    {
+        return self::where('user_id', $userId)
+            ->column('follow_user_id');
+    }
+
+    /**
+     * 获取用户的粉丝数
+     *
+     * @param int $userId
+     * @return int
+     */
+    public static function getFollowerCount($userId)
+    {
+        return self::where('follow_user_id', $userId)->count();
+    }
+
+    /**
+     * 获取用户的关注数
+     *
+     * @param int $userId
+     * @return int
+     */
+    public static function getFollowingCount($userId)
+    {
+        return self::where('user_id', $userId)->count();
+    }
     
-
-
-
-
-
-
-
 }
