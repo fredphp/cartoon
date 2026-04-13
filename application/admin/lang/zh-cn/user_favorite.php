6c18a539738867472263ff1f3928b27a76ae5cf4
fred <fredphp52@gmail.com>
2026-04-13 12:48:25 +0800

    收藏

---
+++ application/admin/lang/zh-cn/user_favorite.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Id'         => 'ID',
+    'User_id'    => '用户ID',
+    'Comic_id'   => '漫画ID',
+    'Createtime' => '创建时间',
+    'Updatetime' => '更新时间'
+];
--- application/common/model/UserFavorite.php
+++ application/common/model/UserFavorite.php
@@ -4,19 +4,30 @@ namespace app\common\model;
 
 use think\Model;
 
-/**
- * 用户收藏模型
- */
+
 class UserFavorite extends Model
 {
-    // 表名（使用 mha_ 前缀，独立于 fa_ 前缀，需显式声明）
-    protected $table = 'mha_user_favorite';
 
-    // 开启自动写入时间戳字段
-    protected $autoWriteTimestamp = 'int';
+    
+
+    
+
+    // 表名
+    protected $name = 'user_favorite';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = 'integer';
+
     // 定义时间戳字段名
     protected $createTime = 'createtime';
     protected $updateTime = 'updatetime';
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [
+
+    ];
+    
 
     /**
      * 关联漫画
@@ -28,10 +39,6 @@ class UserFavorite extends Model
 
     /**
      * 判断用户是否已收藏某漫画
-     *
-     * @param int $userId
-     * @param int $comicId
-     * @return bool
      */
     public static function hasFavorited($userId, $comicId)
     {
@@ -42,13 +49,11 @@ class UserFavorite extends Model
 
     /**
      * 获取用户收藏的漫画ID列表
-     *
-     * @param int $userId
-     * @return array
      */
     public static function getFavoritedIds($userId)
     {
         return self::where('user_id', $userId)
             ->column('comic_id');
     }
+
 }
