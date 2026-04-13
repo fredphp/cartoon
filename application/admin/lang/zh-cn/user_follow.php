92cef9168e5fcef0e44360f282121be42c3aec48
fred <fredphp52@gmail.com>
2026-04-13 12:49:37 +0800

    关注

---
+++ application/admin/lang/zh-cn/user_follow.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Id'             => 'ID',
+    'User_id'        => '用户ID（关注者）',
+    'Follow_user_id' => '被关注的用户ID（作者）',
+    'Createtime'     => '创建时间',
+    'Updatetime'     => '更新时间'
+];
---
+++ public/assets/js/backend/user_follow.js
@@ -0,0 +1,54 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'user_follow/index' + location.search,
+                    add_url: 'user_follow/add',
+                    edit_url: 'user_follow/edit',
+                    del_url: 'user_follow/del',
+                    multi_url: 'user_follow/multi',
+                    import_url: 'user_follow/import',
+                    table: 'user_follow',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'user_id', title: __('User_id')},
+                        {field: 'follow_user_id', title: __('Follow_user_id')},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
